ae47ce8747e8d918cd8f80ab17a9ab864e269d3f
Hangil Chang <hangil2010@naver.com>
2025-06-23 14:26:03 +0900

    [Feat] 플레이어 배고픔 변화 시스템 추가
    - SetHunger(변화량 퍼센트)를 통해 배고픔 수치를 퍼센트 단위로 변화할 수 있음
    - 배고픔 게이지가 0 이하가 될 경우 이동속도,행동속도가 50% 감소하는 허기 디버프 부여
    - 배고픔 게이지가 다시 0 이상이 될 경우 디버프는 해제
    - 행동속도는 기본 설정이 1, 50% 감소할 경우 시간이 2배 걸리기 위해 1 / 디버프 수치로 우선 설정

--- Assets/GIL/Scripts/PlayerStatus.cs
+++ Assets/GIL/Scripts/PlayerStatus.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class PlayerStatus : MonoBehaviour
 {
@@ -17,14 +14,28 @@ public class PlayerStatus : MonoBehaviour
     public float MaxHealth;
     public float CurHealth;
     // 배고픔
+    [Header("Hunger")]
     public float MaxHunger;
     public float CurHunger;
+    [SerializeField] float moveSpeedDescAmount = 0.5f;
+    [SerializeField] float actionSpeedDescAmount = 0.5f;
+    private bool isStarving;
     // 갈증
+    [Header("Thirst")]
     public float MaxThirst;
     public float CurThirst;
     // 정신력
+    [Header("Mentality")]
     public float MaxMentality;
     public float CurMentality;
+    [Header("Speed")]
+    // 이동속도
+    // TODO: 추후에 이동 구현 시 이동속도 관련 코드에다가 옮길지 고민하기.
+    public float MaxPlayerMoveSpeed = 2;
+    public float CurPlayerMoveSpeed;
+    // 행동속도
+    // 추후에 행동을 할 때 속도 배율로 사용할 예정.
+    public float ActionSpeed = 1;
 
     // 싱글톤 검정
     private void Awake() => Init();
@@ -35,13 +46,11 @@ public class PlayerStatus : MonoBehaviour
         CurHunger = MaxHunger;
         CurThirst = MaxThirst;
         CurMentality = MaxMentality;
+        CurPlayerMoveSpeed = MaxPlayerMoveSpeed;
     }
 
     /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    /// <summary>
-    /// 플레이어 체력에 변화, 체력이 0이 될경우 플레이어 사망
+    /// 체력 스텟의 변화, 체력이 0이 될경우 플레이어 사망
     /// </summary>
     /// <param name="value">변화할 체력의 퍼센트 float값</param>
     // 능력치들이 전부 퍼센트로 변화하기 때문에 이 방식을 채택
@@ -52,12 +61,36 @@ public class PlayerStatus : MonoBehaviour
         float deltaValue = MaxHealth * value;
         CurHealth += deltaValue;
         if (CurHealth >= MaxHealth) CurHealth = MaxHealth;
-        if (CurHealth <= 0) PlayerDeath();
-        Debug.Log(CurHealth);
+        if (CurHealth <= 0f)
+        {
+            PlayerDeath();
+            CurHealth = 0f;
+        }
     }
-
-    // 배고픔을 관리하는 로직
-    //public void Set
+    /// <summary>
+    /// 배고픔 스텟의 변화, 체력이 0이 될 경우 허기(이동속도 -50% , 행동속도 -50%)발동
+    /// </summary>
+    /// <param name="value">변화할 체력의 퍼센트 float값</param>
+    // 배고픔이 0 이하가 될 경우 허기 디버프 발동
+    // 배고픔이 다시 0 이상이 될 경우 허기 디버프 해체.
+    public void SetHunger(float value)
+    {
+        float deltaValue = MaxHunger * value;
+        CurHunger += deltaValue;
+        if (CurHunger >= MaxHealth) CurHunger = MaxHunger;
+        if (CurHunger >= 0f && isStarving == true)
+        {
+            CurPlayerMoveSpeed = MaxPlayerMoveSpeed;
+            ActionSpeed = 1;
+            isStarving = false;
+        }
+        if (CurHunger <= 0f && isStarving == false)
+        {
+            StarvationDebuff(moveSpeedDescAmount, actionSpeedDescAmount);
+            isStarving = true;
+            CurHunger = 0f;
+        }
+    }
     // 갈증을 관리하는 로직
     // 정신력을 관리하는 로직
     // 플레이어 사망시 처리되는 로직들
@@ -65,7 +98,12 @@ public class PlayerStatus : MonoBehaviour
     {
         Debug.Log("플레이어 사망!");
     }
-
+    private void StarvationDebuff(float moveSpeed, float actionSpeed)
+    {
+        Debug.Log("굶주렸다..");
+        CurPlayerMoveSpeed -= MaxPlayerMoveSpeed * moveSpeed;
+        ActionSpeed /= actionSpeed;
+    }
     private void Init()
     {
         if (Instance != null && Instance != this)
