254d280156070af6fb5c7cf74728fd26226de634
Hangil Chang <hangil2010@naver.com>
2025-06-23 14:27:57 +0900

    [Test] 플레이어 스텟 테스트 코드 변경
    Q, A 를 통해 체력을 상승, 감소
    W, S 를 통해 배고픔을 상승, 감소할 수 있게 설정
    
    다른 스텟들이 추가될 경우 (E,D), (R,F) 이렇게 추가할 계획

--- Assets/GIL/Scripts/PlayerStatusInstanceTest.cs
+++ Assets/GIL/Scripts/PlayerStatusInstanceTest.cs
@@ -8,7 +8,27 @@ public class PlayerStatusInstanceTest : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
+            PlayerStatus.Instance.SetHealth(+0.1f);
+            Debug.Log($"현재 체력 : {PlayerStatus.Instance.CurHealth}");
+        }if (Input.GetKeyDown(KeyCode.A))
+        {
             PlayerStatus.Instance.SetHealth(-0.1f);
+            Debug.Log($"현재 체력 : {PlayerStatus.Instance.CurHealth}");
         }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            PlayerStatus.Instance.SetHunger(0.1f);
+            Debug.Log($"현재 배고픔 : {PlayerStatus.Instance.CurHunger}");
+            Debug.Log($"현재 이동속도 : {PlayerStatus.Instance.CurPlayerMoveSpeed}");
+            Debug.Log($"현재 행동속도 : {PlayerStatus.Instance.ActionSpeed}");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            PlayerStatus.Instance.SetHunger(-0.1f);
+            Debug.Log($"현재 배고픔 : {PlayerStatus.Instance.CurHunger}");
+            Debug.Log($"현재 이동속도 : {PlayerStatus.Instance.CurPlayerMoveSpeed}");
+            Debug.Log($"현재 행동속도 : {PlayerStatus.Instance.ActionSpeed}");
+        }
+        
     }
 }
