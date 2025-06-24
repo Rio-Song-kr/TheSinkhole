90a50cf46a93d5ab8f1bb5f6d5e2d81edaeaf506
Hangil Chang <hangil2010@naver.com>
2025-06-24 11:37:50 +0900

    [Test] 슬라이더 UI 테스트 제작
    슬라이더를 꽉 채우고, 슬라이더가 0이 될 경우 채우는 부분이 보이지 않게 설정

--- Assets/GIL/Scripts/Test/StatusUITest.cs
+++ Assets/GIL/Scripts/Test/StatusUITest.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 using UnityEngine.UI;
 public class StatusUITest : MonoBehaviour
 {
+    [Header("Text UI")]
     [SerializeField] TextMeshProUGUI curHealthUIText;
     [SerializeField] TextMeshProUGUI curHungerUIText;
     [SerializeField] TextMeshProUGUI curThirstUIText;
     [SerializeField] TextMeshProUGUI curMentalityUIText;
+    [Header("Slider UI")]
+    [SerializeField] Slider sliderTest;
+    [SerializeField] GameObject sliderFill;
+
+    [Header("Image UI")]
+    [SerializeField] Image imageTest;
     /// <summary>
     /// OnGUI is called for rendering and handling GUI events.
     /// This function can be called multiple times per frame (one call per event).
@@ -19,5 +26,14 @@ public class StatusUITest : MonoBehaviour
         curHungerUIText.text = PlayerStatus.Instance.CurHunger.ToString();
         curThirstUIText.text = PlayerStatus.Instance.CurThirst.ToString();
         curMentalityUIText.text = PlayerStatus.Instance.CurMentality.ToString();
+        StatusSlider();
+    }
+
+    void StatusSlider()
+    {
+        float value = PlayerStatus.Instance.CurHunger / PlayerStatus.Instance.MaxHunger;
+        if (value <= 0) sliderFill.SetActive(false);
+        else sliderFill.SetActive(true);
+        sliderTest.value = value;
     }
 }
