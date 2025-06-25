9b32a6f3001e38a72e93fea26deb47efdeaf05cb
이하나 <45592710+leehana0921@users.noreply.github.com>
2025-06-25 09:40:12 +0900

    [Fix] 게임 시간 기능 수정
    하루가 지나면 며칠인지 표시 되게끔 day 추가, AM, PM으로 구분 되게 수정, 시간만 표시 되게끔 수정

--- Assets/LHN/Scripts/GameTimer.cs
+++ Assets/LHN/Scripts/GameTimer.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using System.Globalization;
 
 public class GameTimer : MonoBehaviour
 {
     private const double GameMultiplier = 3600.0 / 30.0;
     private DateTime realStartTime;
-
+    int count = 1;
     public TextMeshProUGUI gameTimeText; // UI에 연결할 텍스트
 
     void Start()
@@ -15,7 +16,7 @@ public class GameTimer : MonoBehaviour
         // 게임 시작 시점 기록
         realStartTime = DateTime.Now;
     }
-
+    DateTime beforeTime = new DateTime(1, 1, 1, 6, 0, 0);
     void Update()
     {
         DateTime now = DateTime.Now;
@@ -25,9 +26,14 @@ public class GameTimer : MonoBehaviour
         // 기준이 되는 6시부터 흐른 게임 시간 생성
         DateTime gameTime = new DateTime(1, 1, 1, 6, 0, 0).AddSeconds(gameSeconds);
 
-        string realTimeFormatted = now.ToString("tt hh:mm");
-        string gameTimeFormatted = gameTime.ToString("tt hh:mm");
+        string gameTimeFormatted = gameTime.ToString("tt hh", CultureInfo.GetCultureInfo("en-US"));
 
-        gameTimeText.text = $"현실 시간: {realTimeFormatted}\n게임 시간: {gameTimeFormatted}";
+        if (gameTimeFormatted == "AM 06" && beforeTime.ToString("tt hh", CultureInfo.GetCultureInfo("en-US")) == "AM 05")
+        {
+            count += 1;
+        }
+        gameTimeText.text = $"DAY : {count}, {gameTimeFormatted}";
+        // 조건문 시간 비교를 위한 이전 시간 저장
+        beforeTime = gameTime;
     }
 }
