4a6eabc3e48b04ae9c5df92103dae0a24c269f23
99jinmo <99jinmo@gmail.com>
2025-06-27 12:28:37 +0900

    [Feat] 제작 시스템
    필요 재료 보이게 됨
    바탕화면 누르면 제작 꺼짐
    레시피 선택 시 레시피에 맞는 제작정보 나옴

--- Assets/JJM/Scripts/CraftingButton.cs
+++ Assets/JJM/Scripts/CraftingButton.cs
@@ -7,12 +7,17 @@ namespace CraftingSystem
     // UI 버튼에서 제작을 실행하는 스크립트
     public class CraftingButton : MonoBehaviour
     {
-        public CraftingRecipe recipe;// 연결할 레시피
+        public RecipePanelController recipePanelController; // 패널 컨트롤러 참조
+
         public CraftingManager craftingManager;// 매니저 참조
 
         public void OnClickCraft()
         {
-            craftingManager.TryCraftWithDelay(recipe);// 제작 시도(제작 시간 적용)
+            var recipe = recipePanelController.GetCurrentRecipe();
+            if (recipe != null)
+                craftingManager.TryCraftWithDelay(recipe);
+            else
+                Debug.LogWarning("선택된 레시피가 없습니다!");
 
         }
     }
