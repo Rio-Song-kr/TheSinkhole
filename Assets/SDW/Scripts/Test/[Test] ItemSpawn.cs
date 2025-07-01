ccb8d5640f8ff8eda0f9973a2e8076e47ee28d66
Rio_Song <rio.dw.song@gmail.com>
2025-07-01 15:40:46 +0900

    [Fix] TestItemSpawn 수정
    * [Test] ItemSpawn 아래에 아이템이 생성되도록 변경

--- Assets/SDW/Scripts/Test/[Test] ItemSpawn.cs
+++ Assets/SDW/Scripts/Test/[Test] ItemSpawn.cs
@@ -9,7 +9,7 @@ public class TestItemSpawn : MonoBehaviour
         for (int i = 0; i < 10; i++)
         {
             var herb = GameManager.Instance.Item.ItemPools[ItemEnName.HerbPill].Pool.Get();
-            herb.transform.parent = transform.parent;
+            herb.transform.parent = transform;
             herb.ItemAmount = 500;
             m_position += Vector3.forward * 2f;
             herb.transform.position = m_position;
@@ -18,19 +18,19 @@ public class TestItemSpawn : MonoBehaviour
         m_position += Vector3.right * 2f;
 
         var shovel = GameManager.Instance.Item.ItemPools[ItemEnName.Shovel].Pool.Get();
-        shovel.transform.parent = transform.parent;
+        shovel.transform.parent = transform;
         shovel.ItemAmount = 1;
         m_position -= Vector3.forward * 2f;
         shovel.transform.position = m_position;
 
         var hammer = GameManager.Instance.Item.ItemPools[ItemEnName.Hammer].Pool.Get();
-        hammer.transform.parent = transform.parent;
+        hammer.transform.parent = transform;
         hammer.ItemAmount = 1;
         m_position -= Vector3.forward * 2f;
         hammer.transform.position = m_position;
 
         var pick = GameManager.Instance.Item.ItemPools[ItemEnName.Pick].Pool.Get();
-        pick.transform.parent = transform.parent;
+        pick.transform.parent = transform;
         pick.ItemAmount = 1;
         m_position -= Vector3.forward * 2f;
         pick.transform.position = m_position;
