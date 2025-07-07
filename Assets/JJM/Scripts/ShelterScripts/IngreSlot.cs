546b744746b1dbdbdc7b319dc2d38bf8b41aa666
99jinmo <99jinmo@gmail.com>
2025-07-07 11:28:30 +0900

    [Feat] 업그레이드 재료 연결
    업그레이드UI에 필요재료 뜨게 작업

--- Assets/JJM/Scripts/ShelterScripts/IngreSlot.cs
+++ Assets/JJM/Scripts/ShelterScripts/IngreSlot.cs
@@ -10,8 +10,9 @@ public class IngreSlot : MonoBehaviour
     public TextMeshProUGUI nameText; // 재료 이름 텍스트
     public TextMeshProUGUI quantityText; // 재료 수량 텍스트
 
-    public void Set(string name, int have, int need)
+    public void Set(Sprite icon,string name, int have, int need)
     {
+        iconImage.sprite = icon; // 재료 아이콘 설정
         nameText.text = name; // 재료 이름 설정
         quantityText.text = $"{have} / {need}"; // 필요재료/소지재료 수량 설정
         quantityText.color = have >= need ? Color.white : Color.red; // 수량에 따라 색상 변경
